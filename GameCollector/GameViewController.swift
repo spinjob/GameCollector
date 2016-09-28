4953c251c9a1f2e5670228e6cf3ea750834a0b44
Spencer Johnson <spencer.johnson10@gmail.com>
2016-09-27 22:49:40 -0400

    Added GameViewController and layout.  ImagePicker view implemented and is assigned to the image in the gameImageView.

---
+++ GameCollector/GameViewController.swift
@@ -0,0 +1,68 @@
+//
+//  GameViewController.swift
+//  GameCollector
+//
+//  Created by Spencer Johnson on 9/27/16.
+//  Copyright © 2016 Spencer Johnson. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    
+    @IBOutlet weak var gameImageView: UIImageView!
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    var imagePicker = UIImagePickerController ()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imagePicker.delegate = self
+        
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func photosTapped(_ sender: AnyObject) {
+        imagePicker.sourceType = .photoLibrary
+    
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        gameImageView.image = image
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func cameraTapped(_ sender: AnyObject) {
+    }
+    
+    @IBAction func addTapped(_ sender: AnyObject) {
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
